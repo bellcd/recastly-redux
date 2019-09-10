7ee7d934c64dc90d65fd833b8f3012fd155146d8
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-09-11 02:07:16 +0700

    Defines basic action creators

--- src/actions/search.js
+++ src/actions/search.js
@@ -5,8 +5,27 @@ import YOUTUBE_API_KEY from '../config/youtube.js';
 
 
 var handleVideoSearch = (q) => {
- 
   //TODO:  Write an asynchronous action to handle a video search!
+
+  let obj = {
+    query: q,
+    key: YOUTUBE_API_KEY
+  };
+
+  return function(dispatch) {
+    return searchYouTube(obj, (data) => {
+      debugger;
+      console.log('we got to search you tube')
+      // console.log(changeVideoList(data))
+      dispatch(changeVideoList(data));
+      dispatch(changeVideo(data[0]));
+    });
+  }
 };
 
 export default handleVideoSearch;
+
+// changeVideoList = searchYoutTube(q)
+// new messages
+// {type:}
+
